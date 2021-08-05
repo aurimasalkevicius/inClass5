7c11e89de642ec6e3224f3f1cc18bc39ff7acf52
Alkevicius <aurimasalkevicius@gmail.com>
2021-08-05 06:44:18 -0400

    initial

--- js/example.js
+++ js/example.js
@@ -1,20 +1,24 @@
 // ADD NEW ITEM TO END OF LIST
+
 var div = document.getElementById("page");
 var li = document.createElement("li");
 li.appendChild(document.createTextNode("cream"));
-div.append(li);
 
 // ADD NEW ITEM START OF LIST
 
-console.log(div);
+// Could not figure out how to add this item to the top of the list without id
+// it looks like the selectElementsById returns HTMLcollection which does not 
+// have append() method.
+
+// Could you please let me know what is the code snippet that I am missing
+
 li = document.createElement("li");
 li.appendChild(document.createTextNode("kale"));
-
 var list = document.getElementsByTagName("ul")[0];
-console.log(list);
 list.append(li);
 
 // ADD A CLASS OF COOL TO ALL LIST ITEMS
+
 var elm = document.getElementsByTagName('li')
 var length = elm.length;
 for (var i = 0; i < length; i++) {
@@ -22,12 +26,7 @@ for (var i = 0; i < length; i++) {
 }
 
 // ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+
 var numOfItems = document.querySelectorAll("li").length
 var element = document.querySelectorAll("h2")[0];
-console.log(element)
-
-
-//var ele = div.nextElementSibling.nextElementSibling;
-
-console.log(div);
 element.innerHTML += "<span>" + numOfItems + "</span>";
